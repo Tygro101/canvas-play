7956508f28bb05f8985224268d78f1400c6df150
Ran Tayeb <Tayebran@gmail.com>
2020-12-09 20:26:25 +0200

    Update circle.shape.ts

--- src/app/components/objects/circle.shape.ts
+++ src/app/components/objects/circle.shape.ts
@@ -3,11 +3,13 @@ import { BaseShape, Shape } from './base.shape';
 
 
 export class CircleShape extends BaseShape implements Shape {
+
     radius: number = Math.random() * 5 + 2;
     speed: number = Math.random() * 1.2 + 0.2;
     xDirection: number = Math.random() >= 0.5 ? 1 : -1;
     yDirection: number = Math.random() >= 0.5 ? 1 : -1;
-    type: CircleType = Math.random() > 0.95 ? CircleType.RED : CircleType.WHITE;
+    type: CircleType = this.randType();
+    alpha: number = Math.random() * 2 * Math.PI;
     mouseLocation: MouseLocation;
     lineDistance: number;
     constructor(updateManager: UpdateManagerService, context2D: CanvasRenderingContext2D) {
@@ -18,13 +20,24 @@ export class CircleShape extends BaseShape implements Shape {
         updateManager.mouseLocationChanged.subscribe((mouseLocation) => {
             this.mouseLocation = mouseLocation;
         });
-        this.lineDistance = this.type === CircleType.WHITE ? 150 : 500;
+        this.lineDistance = this.getDistance();
     }
 
     update(): void {
-        this.updateLocation();
+        const shouldDrawLine = this.checkLine();
+        if (shouldDrawLine) {
+            this.drawLine();
+            this.followMouse();
+        } else {
+            this.updateLocation();
+        }
         this.drawCircle();
-        this.checkLine();
+        //this.checkLine();
+    }
+    followMouse() {
+        const alpha = Math.atan2(this.mouseLocation.y - this.y, this.mouseLocation.x - this.x);
+        this.x += Math.cos(alpha) * this.speed;
+        this.y += Math.sin(alpha) * this.speed;
     }
     drawCircle() {
         this.context2D.beginPath();
@@ -33,24 +46,24 @@ export class CircleShape extends BaseShape implements Shape {
         this.context2D.fill();
     }
     updateLocation() {
-        this.x += this.speed * this.xDirection;
-        this.y += this.speed * this.yDirection;
+        this.x += this.speed * this.xDirection * Math.cos(this.alpha);
+        this.y += this.speed * this.yDirection * Math.sin(this.alpha);
         this.checkDirection();
     }
-    checkLine() {
+    checkLine(): boolean {
         if (this.mouseLocation) {
             const x = Math.abs(this.x - this.mouseLocation.x);
             const y = Math.abs(this.y - this.mouseLocation.y);
             const dist = Math.sqrt(x * x + y * y);
             if (dist < this.lineDistance) {
-                this.drawLine();
+                return true;
             }
         }
     }
     drawLine() {
         const grad = this.context2D.createLinearGradient(this.x, this.y, this.mouseLocation.x, this.mouseLocation.y);
         grad.addColorStop(0, this.getColor());
-        grad.addColorStop(1, "#70ff9d");
+        grad.addColorStop(1, "#ffffff");
         this.context2D.strokeStyle = grad;
         this.context2D.beginPath();
         this.context2D.moveTo(this.x, this.y);
@@ -71,8 +84,32 @@ export class CircleShape extends BaseShape implements Shape {
         switch (this.type) {
             case CircleType.WHITE:
                 return "#ffffff";
-            case CircleType.RED:
+            case CircleType.GREEN:
                 return "#70ff9d";
+            case CircleType.BLUE:
+                return "#36b8ff";
+            case CircleType.PINk:
+                return "#ff66d6";
+        }
+    }
+
+    randType(): CircleType {
+        const rand = Math.random();
+        if (rand > 0.9) return CircleType.GREEN;
+        if (rand > 0.75) return CircleType.PINk;
+        if (rand > 0.6) return CircleType.BLUE;
+        return CircleType.WHITE;
+    }
+
+    getDistance(): number {
+        switch (this.type) {
+            case CircleType.GREEN:
+                return 400;
+            case CircleType.PINk:
+                return 300;
+            case CircleType.BLUE:
+                return 200;
+            default: return 100;
         }
     }
 }
@@ -82,6 +119,8 @@ export class CircleShape extends BaseShape implements Shape {
 
 
 export enum CircleType {
-    RED = "red",
-    WHITE = "white"
+    GREEN = "green",
+    WHITE = "white",
+    BLUE = "blue",
+    PINk = "pink"
 }
